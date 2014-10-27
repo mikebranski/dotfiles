cef9a0584bac96a1cbb2e04c456e0d064dca68f4
Mike Branski <mike.branski@7summitsagency.com>
2014-10-27 16:38:52 -0500

    Add show/hide hidden files aliases

---
+++ osx/aliases.zsh
@@ -0,0 +1,2 @@
+alias showhidden='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
+alias hidehidden='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
