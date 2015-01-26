4543f9b1345e8d12716d70db248edd3e6f1f0180
Mike Branski <mike.branski@7summitsagency.com>
2015-01-26 15:13:02 -0600

    More show/hide alias fun

--- osx/aliases.zsh
+++ osx/aliases.zsh
@@ -1,2 +1,8 @@
 alias showhidden='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
 alias hidehidden='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
+
+alias showallthethings='showhidden'
+alias hideallthethings='hidehidden'
+
+alias satt='showallthethings'
+alias hatt='hideallthethings'
